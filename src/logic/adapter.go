d8fe4702a06b9939248af1c104b46c0f284695d8
ElectRICdll <2636422098@qq.com>
2024-04-24 15:31:54 +0800

    抽象接口定义

--- src/logic/adapter.go
+++ src/logic/adapter.go
@@ -1,5 +1,25 @@
 package logic
 
-type Adapter interface {
+import "fmt"
+
+// Constructor should describe the function to create a Translator.
+type Constructor func() (Translator, error)
+
+// Translator abstracts translate APIs.
+type Translator interface {
 	Translate(string, Prop) (string, error)
+
+	LangSupported() []Lang
+}
+
+// Reg registers a new Constructor with a alias.
+func Reg(alias string, c Constructor) {
+	if manager[alias] != nil {
+		panic(fmt.Sprintf("%s duplicate registered! check your adapters", alias))
+	}
+	manager[alias] = c
+}
+
+func TranslatorFactory(name string) (Translator, error) {
+	return manager[name]()
 }
